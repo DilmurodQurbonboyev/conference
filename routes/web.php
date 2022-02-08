35288b41cf608cf924c6d842a7951e30af471a1e
Dilmurod H. Qurbonboyev <d_qurbonboyev@uzinfocom.local>
2022-02-08 09:54:24 +0500

    s

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::group(
 
 
         Route::get('clear', function () {
-            Artisan::call('optimize:clear');
+//            Artisan::call('optimize:clear');
             Artisan::call('storage:link');
             return redirect('/');
         });
